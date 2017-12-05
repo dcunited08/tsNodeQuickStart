293e570c1b7470118b667d54ea7916387fcd69fb
jdcook <jdcook@barracuda.com>
2017-12-05 17:04:28 -0500

    includes ts

--- dist/function.js
+++ dist/function.js
@@ -1,4 +1,3 @@
-
 /**
  * Responds to any HTTP request that can provide a "message" field in the body.
  *
@@ -9,10 +8,12 @@ exports.helloWorld = function helloWorld(req, res) {
     // Example input: {"message": "Hello!"}
     if (req.body.message === undefined) {
         // This is an error case, as "message" is required.
-        res.status(200).send('Hello World! This is working through GIT');
-    } else {
+        res.status(200).send("Hello World! This is working through GIT");
+    }
+    else {
         // Everything is okay.
         console.log(req.body.message);
-        res.status(200).send('Success: ' + req.body.message);
+        res.status(200).send("Success: " + req.body.message);
     }
 };
+//# sourceMappingURL=function.js.map
