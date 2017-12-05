6e2f288ae443a54d10c4be036fb660f514e08bcf
jdcook <jdcook@barracuda.com>
2017-12-05 16:44:51 -0500

    fsda

--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,11 @@
+
 class Bob {
     hotDog() {
         return "fdsa";
     }
 }
-exports.helloWorld = function helloWorld(req, res) {
+
+exports.helloWorld = function helloWorld(req: any, res: any) {
     const tst = new Bob();
     return tst.hotDog();
 };
-//# sourceMappingURL=function.js.map
